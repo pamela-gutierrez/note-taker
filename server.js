a2ad934671b9fc0975fd95807c5599f301868ec6
pmgtrz <75604818+pamela-gutierrez@users.noreply.github.com>
2021-01-26 09:20:36 -0800

    connect to heroku

--- server.js
+++ server.js
@@ -1,67 +1,74 @@
 // Dependencies aka Packages that give the server functionality
-var express = require("express");
-var fs = require("fs");
-var path = require("path");
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 // Tells node that we are creating an "express" server
-var app = express();
+const app = express();
 
 // Sets an initial port, I'll use this later in our listener.
-var PORT = process.env.PORT || 4040;
+const PORT = process.env.PORT || 4040;
 
-// LOOOK THIS UP BECAUSE I DON'T REALLY UNDERSTAND IT. Sets up the Express app to handle data parsing.
+// LOOOK THIS UP BECAUSE YOU DON'T REALLY UNDERSTAND IT. Sets up the Express app to handle data parsing.
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
-// where all the new notes go
-var newNotes = [];
 
 // ROUTES/REDIRECTS
-// index.html
-app.get("*", function (request, response) {
-    response.sendFile(path.join(__dirname, "index.html"))
-});
-
 // notes.html
 app.get("/notes", function (request, response) {
     response.sendFile(path.join(__dirname, "notes.html"))
 });
 
+// index.html
+app.get("*", function (request, response) {
+    response.sendFile(path.join(__dirname, "index.html"))
+});
+
 // api/display notes
 app.get("/api/notes", function (request, response) {
-    fs.readFile("db/db.json", "utf-8", function (err, data) {
+    fs.readFile("db/db.json", "utf8", function (err, data) {
         if (err) throw err
         return response.json(data);
-    })
+    });
 
 });
 
 app.delete("/api/notes/:id"), function (request, response) {
-    var index = req.body.db
-    response.send("note removed")
+    fs.readFile("db/db.json", "utf8", function (err, data) {
+        if (err) throw err;
+        var index = req.body.index;
+        var newNotes = [];
+        for (let i = 0; i < db.length; i++) {
+            if (i === JSON.parse(i)) {
+                newNote.push(db(i));
+            }
+        }
+        response.send("Note removed!")
+    })
 }
 
 
+// POST/api/notes
+// app.post("/api/notes", function (request, response) {
+//     var newNote = request.body
+//     fs.readFile("db.json", "utf8", function (err, data) {
+//         if (err) throw err;
+//     })
+//     var newNotes = [];
+//     console.log(newNote)
 
+// }),
 
-// // api/delete notes
-// app.put("/api/delete", function (request, response) {
-//     newNotes = [];
-//     response.send("delete")
+// fs.writeFile("db/db.json", newNotes, function (err, data) {
+//     if (err) throw err;
 // })
 
-// POST/api/notes
-app.post("/api/notes", function (request, response) {
-    var newNote = request.body
-    console.log(newNote)
-
-})
-
 // This points the server to a series files that will give the server a "map" of how to respond when users visit or request data from various URLS.
-require("./index.js")(app);
+// require("public/assets/js/index.js");
 
 // Listener: this code "starts" the server.
 app.listen(PORT, function () {
-    console.log("App listening on PORT: " + PORT);
-});
-
+    console.log("App listening on PORT: " + PORT)
+})
